8c417d0ae92963c2e6fba8bfef1c74da532837a2
chumsee <85548852+chumsee@users.noreply.github.com>
2022-12-06 17:48:20 +0400

    Update main.rb

--- Dragon/main.rb
+++ Dragon/main.rb
@@ -2,11 +2,31 @@
 
 require_relative 'head'
 
-head1 = Head.new
-head1.look(:r, 8)
-head1.look(:l, 500)
-head1.look(:f, 11)
-head1.look(:b, 0)
-head1.look(:l, 8)
-p head1.history
-p head1.minutes
+left_head = Head.new
+middle_head = Head.new
+right_head = Head.new
+
+left_head.look(:b, 100)
+left_head.look(:l, 100)
+left_head.look(:f, 100)
+
+middle_head.look(:r, 80)
+middle_head.look(:f, 20)
+middle_head.look(:l, 80)
+middle_head.look(:r, 80)
+middle_head.look(:l, 80)
+middle_head.look(:f, 80)
+
+right_head.look(:b, 50)
+right_head.look(:f, 50)
+right_head.look(:l, 150)
+right_head.look(:r, 50)
+right_head.look(:b, 50)
+right_head.look(:f, 50)
+
+looking_in_one_direction_time = 0
+(1..([left_head.minutes, right_head.minutes, middle_head.minutes].min)).each do |i|
+   looking_in_one_direction_time += 1 if left_head.direction_by_minute(i) == middle_head.direction_by_minute(i) && middle_head.direction_by_minute(i) == right_head.direction_by_minute(i)
+end
+
+p looking_in_one_direction_time
