558fbdba7c56a9574ceb122b23c9af0e81bebfce
chumsee <85548852+chumsee@users.noreply.github.com>
2022-12-06 12:02:31 +0400

    Update head.rb

--- Dragon/head.rb
+++ Dragon/head.rb
@@ -2,25 +2,15 @@
 
 class Head
     
-  attr_accessor :side, :minutes, :history
+  attr_accessor :minutes, :history
 
   def initialize
-    @side = nil
     @minutes = 0
-    @history = []
+    @history = [[nil, @minutes]]
   end
 
   def look(side, minutes)
-    if @history.empty?
-        @history << [side, minutes]
-    else
-        @history << [side, history[-1][1] + minutes]
-    end
-    @side = side
-    @minutes = @history[-1][1]
-  end
-
-  def timeline
-    @history
+    @history << [side, history.last[1] + minutes]
+    @minutes = @history.last[1]
   end
 end
